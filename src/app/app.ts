729cf1c01204f059b04d18eceee16e9bd6977790
Pieter Deparcq <pdeparcq@gmail.com>
2015-10-24 08:04:59 +0200

    first working app

---
+++ src/app/app.ts
@@ -0,0 +1,8 @@
+import {Component, bootstrap} from 'angular2/angular2';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Pieter</h1>'
+})
+class AppComponent { }
+
+bootstrap(AppComponent);
